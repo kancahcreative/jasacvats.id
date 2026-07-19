60c9fab36fae61d625b5566db483d5f226cd975a
kancahcreative <kancahcreative@gmail.com>
2026-07-20 00:50:50 +0700

    Update [slug].js

--- functions/[slug].js
+++ functions/[slug].js
@@ -1,12 +1,18 @@
 export async function onRequest(context) {
   const { request, env } = context;
   const url = new URL(request.url);
+  const pathname = url.pathname;
 
-  // Safety net: kalau ini request ke file asli (css/js/gambar/dll), jangan diganggu
-  if (/\.[a-zA-Z0-9]+$/.test(url.pathname)) {
+  // Route yang sudah ditangani _redirects atau file statis -> biarkan lewat normal
+  const knownRoutes = new Set([
+    '/', '/beranda', '/tentang-kami', '/detail-paket', '/paket-harga', '/faq', '/artikel'
+  ]);
+
+  if (knownRoutes.has(pathname) || /\.[a-zA-Z0-9]+$/.test(pathname)) {
     return context.next();
   }
 
+  // Sisanya baru dianggap slug artikel
   const assetUrl = new URL('/artikel-detail', url.origin);
   return env.ASSETS.fetch(assetUrl.toString());
 }
